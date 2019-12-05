ec2b56964b8cc519d78f318edcf97f0990b07b60
Senthil & Rahul <vsenth17@ford.com & hrahul1@ford.com>
2019-12-05 23:31:54 +0530

    Trying conditionla check to insert

--- server/routes/index.js
+++ server/routes/index.js
@@ -34,7 +34,7 @@ module.exports = (app) => {
     const result = entryAccessDetailsController.list(req, res);
     result.then(data => {
         console.log("console: "+data)
-        console.log("console1: "+data.data)
+        console.log("console1: "+data[0])
     });
             });
   app.get('/api/entryAccess', entryAccessDetailsController.list);
